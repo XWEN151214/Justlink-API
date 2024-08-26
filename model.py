e7c95a500c676d430b49e3c1cd1de080bbdc0b3d
XWEN <140294243+xwenco@users.noreply.github.com>
2024-08-26 05:44:14 +0500

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
 
-API_KEY = "AIzaSyDxx8Po9zoJr0UGa2p8ybnI4PPlzMVwE_U"
+API_KEY = ""
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash-latest")
