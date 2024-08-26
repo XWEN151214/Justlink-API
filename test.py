18d57eb18c6d114ac1406f070e768db3b88f1f91
XWEN <140294243+xwenco@users.noreply.github.com>
2024-08-26 05:43:46 +0500

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def to_markdown(text):
 response = requests.get("https://www.geeksforgeeks.org/python-web-scraping-tutorial/")
 soup = BeautifulSoup(response.content, 'html.parser')
 body = soup.find('body').text.strip()
-API_KEY = "AIzaSyDxx8Po9zoJr0UGa2p8ybnI4PPlzMVwE_U"
+API_KEY = "
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash-latest")
 content = model.generate_content(f"Here is the content of the blog/article: {body}, Please analyze the following blog/article content and generate a concise and effective report. Focus on summarizing the key points, insights, and main arguments presented. Exclude any extraneous information, such as hyperlinks, advertisements, unrelated sections, or promotional content. Ensure the report is well-organized, clear, and captures the essence of the original content without any unnecessary details.")
